21af862aba9d4243b6d16fc91a1453c58bcf0db7
ranajparida89 <ranajparida89@gmail.com>
2025-03-28 14:44:46 +0530

    UI redesign with dark theme and leaderboard fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+// index.js
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css';         // Existing base styles
+import './styles/theme.css';  // 🆕 Custom dark theme styling
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -12,7 +14,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Performance reporting (optional)
 reportWebVitals();
