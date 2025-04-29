954c59c8488ac9178777b8e67817704fcf050494
ranajparida89 <ranajparida89@gmail.com>
2025-04-30 01:29:15 +0530

    Upcomming Match UI code x

--- src/components/SidebarMenu.js
+++ src/components/SidebarMenu.js
@@ -33,6 +33,8 @@ const SidebarMenu = () => {
         <li><Link to="/squad-lineup" onClick={handleClose}><FaUsers className="me-2" /> Squad / Lineup</Link></li>
         <li><Link to="/qualification-scenario" onClick={handleClose}><FaChartLine className="me-2" /> Qualification Scenario</Link> {/* NEW */}</li>
         <li><Link to="/add-upcoming-match" onClick={handleClose}><FaPlus className="me-2" /> Add Upcoming Match</Link></li>
+        <li><Link to="/upcoming-matches" onClick={handleClose}><FaChartLine className="me-2" /> Upcoming Match Details</Link> {/* NEW */}</li>
+
       </ul>
     </div>
   );
